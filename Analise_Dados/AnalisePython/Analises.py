578d71c636c93ef3a57fd55069282eaeef41db0a
ZePedroFernandes <josefernandesaiao@gmail.com>
2023-05-03 21:50:47 +0100

    Commit graphs

---
+++ Analise_Dados/AnalisePython/Analises.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+    
+def outliersSimple(df: pd.DataFrame, column_to_filter: str, filterValue: int):
+    _, axs = plt.subplots(1, 2)
+
+    axs[0].set_title(f"{column_to_filter} (prev)")
+    axs[0].boxplot(df[column_to_filter])
+
+    filtered = df[df[column_to_filter] <= filterValue]
+    axs[1].set_title(f"{column_to_filter} <= {filterValue}")
+    axs[1].boxplot(filtered[column_to_filter])
+
+    plt.show()
+
+def outliersWithPieChart(df: pd.DataFrame, column_to_filter: str, filterValue: int):
+    outliersSimple(df, column_to_filter, filterValue)
+
+    counts = df[column_to_filter].value_counts()
+
+    count = counts.to_list()
+    index = counts.index.to_list()
+
+    filtered = df[df[column_to_filter] <= filterValue]
+    counts2 = filtered[column_to_filter].value_counts()
+
+    count2 = counts2.to_list()
+    index2 = counts2.index.to_list()
+
+    _, axs2 = plt.subplots(1, 2)
+
+    nl1 = []
+    for i in range(len(count)):
+        s = f"{index[i]} - {count[i]}"
+        nl1.append(s)
+
+    nl2 = []
+    for i in range(len(count2)):
+        s = f"{index2[i]} - {count2[i]}"
+        nl2.append(s)
+
+    axs2[0].pie(counts, autopct='%1.0f%%')
+
+    # move legend to the right
+    axs2[0].legend(nl1, loc='right', bbox_to_anchor=(1.5,0.5))
+
+    axs2[1].pie(counts2, autopct='%1.0f%%')
+
+    # move legend to the right
+    axs2[1].legend(nl2, loc='right', bbox_to_anchor=(1.5,0.5))
+
+
+    plt.show()
--- Analise_Dados/AnalisePython/main.py
+++ Analise_Dados/AnalisePython/main.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import math
+import Analises as an
 
-hotel = pd.read_csv("../DatasetOriginais/Hotel.csv", delimiter=';')
-facilities = pd.read_csv("../DatasetOriginais/Facilities.csv", delimiter=';')
-reservas = pd.read_csv("../DatasetOriginais/Quartos_Reservados.csv", delimiter=';')
-tipologias = pd.read_csv("../DatasetOriginais/Tipologias.csv", delimiter=';')
-eventos = pd.read_csv("../DatasetOriginais/Eventos.csv", delimiter=';')
-meteorologia = pd.read_csv("../DatasetOriginais/Meteorologia.csv", delimiter=';')
-feriados = pd.read_csv("../DatasetOriginais/Feriados.csv", delimiter=';')
-final = pd.read_csv("../DataSetFinal.csv", delimiter=";")
+num_noites = "Número de noites"
+ocupacao = "Ocupação"
+adultos = "Adultos"
+criancas = "Crianças"
+bebes = "Bebés"
+preco = "Preço (€)"
 
-fig, axs = plt.subplots(1, 2)
+hotel = pd.read_csv("./Analise_Dados/DatasetOriginais/Hotel.csv", delimiter=';')
+facilities = pd.read_csv("./Analise_Dados/DatasetOriginais/Facilities.csv", delimiter=';')
+reservas = pd.read_csv("./Analise_Dados/DatasetOriginais/Quartos_Reservados.csv", delimiter=';')
+tipologias = pd.read_csv("./Analise_Dados/DatasetOriginais/Tipologias.csv", delimiter=';')
+eventos = pd.read_csv("./Analise_Dados/DatasetOriginais/Eventos.csv", delimiter=';')
+meteorologia = pd.read_csv("./Analise_Dados/DatasetOriginais/Meteorologia.csv", delimiter=';')
+feriados = pd.read_csv("./Analise_Dados/DatasetOriginais/Feriados.csv", delimiter=';')
+final = pd.read_csv("./Analise_Dados/DataSetFinal.csv", delimiter=";")
 
-df = reservas[reservas['Número de noites'] <= 14]
-
-axs[0].boxplot(df["Número de noites"])
-axs[1].boxplot(reservas["Número de noites"])
-
-plt.show()
+an.analise1(reservas, preco, 20684)
